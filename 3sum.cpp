27a65dd3b53d8a3866206d4a904a6cfdbd7a78cf
Vinton Sistoza <tsistoza@gmail.com>
2024-10-17 13:00:14 -1000

    3sum.cpp LeetCode

---
+++ 3sum.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+
+using std::cout, std::cin, std::endl;
+using std::vector, std::unordered_map, std::pair;
+using std::make_pair;
+
+static vector<int> nums = {-1,0,1,2,-1,-4};
+
+class Solution {
+    public:
+        vector<vector<int>> threeSum(vector<int>& nums);
+        void createHashMap(unordered_map<int, vector<int>>& map, vector<int>& nums);
+        void readMap(unordered_map<int, vector<int>>& map);
+        void readResult(vector<vector<int>>& triplets);
+        pair<bool, int> checkRepeat(int index1, int index2, vector<int>& val);
+};
+
+vector<vector<int>> Solution::threeSum(vector<int>& nums) {
+    int index3;
+    vector<vector<int>> triplets;
+    vector<int> triples;
+    unordered_map<int, vector<int>> hashMap;
+    createHashMap(hashMap, nums);
+    readMap(hashMap);
+    for (int i=0 ;i<nums.size()-1 ;i++) {
+        for (int j=i+1; j<nums.size()-1 ;j++) {
+            int index3 = -nums[i]-nums[j];
+            cout << "i = " << nums[i] << " j = " << nums[j] << " k = " << index3 << endl;
+            auto search = hashMap.find(index3);
+            pair<bool,int> p;
+            if (search != hashMap.end()) {
+                p = checkRepeat(i, j, search->second);
+                if (p.first) {
+                    triples.push_back(nums[i]);
+                    triples.push_back(nums[j]);
+                    triples.push_back(nums[p.second]);
+                    triplets.push_back(triples);
+                    triples.clear();
+                }
+            }
+        }
+    }
+    return triplets;
+}
+
+void Solution::createHashMap(unordered_map<int, vector<int>>& map, vector<int>& nums) {
+    for (int i=0 ;i<nums.size()-1 ; i++){
+        if(map.find(nums[i]) == map.end()) map.insert({nums[i], {i}});
+        else map.find(nums[i])->second.push_back(i);
+    }
+    return;
+}
+
+void Solution::readMap(unordered_map<int, vector<int>>& map) {
+    cout << "Hash Map" << endl;
+    for (auto it = map.begin(); it != map.end(); it++) {
+        cout << "{" << it->first << ", {";
+        for (auto it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+            cout << *it2 << ", ";
+        }
+        cout << "}}" << endl;
+    }
+    return;
+}
+
+void Solution::readResult(vector<vector<int>>& triplets) {
+    cout << "Triplets: { {";
+    for(vector<vector<int>>::iterator it=triplets.begin(); it!=triplets.end(); it++) {
+        for(vector<int>::iterator it2=it->begin(); it2!=it->end(); it2++) {
+            cout << *it2 << ", ";
+        }
+        cout << "}, {" << endl;
+    }
+    cout << "}" << endl;
+    return;
+}
+
+pair<bool,int> Solution::checkRepeat(int index1, int index2, vector<int>& val) {
+    bool result;
+    int value;
+    for (vector<int>::iterator it=val.begin(); it!=val.end(); it++) {
+        if (*it != index1 && *it != index2) {
+            value = *it;
+            result = true;
+            break;
+        } else {
+            result = false;
+            value = (int)NULL;
+        }
+    }
+    pair<bool,int> p {result, value};
+    return p;
+}
+
+int main() {
+    Solution object;
+    vector<vector<int>> result = object.threeSum(nums);
+    object.readResult(result);
+    return 0;
+}
