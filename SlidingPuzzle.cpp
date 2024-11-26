fed04894aa2d11844d349d7a0531d26b76c3cc30
Vinton Sistoza <tsistoza@gmail.com>
2024-11-25 15:51:18 -1000

    SlidingPuzzle.cpp 773
    LeetCode 773
    Took me a while, the first time I did it, I did not know you have to return the least amount, but now it should be good.

--- SlidingPuzzle.cpp
+++ SlidingPuzzle.cpp
@@ -23,43 +23,56 @@ static vector<vector<int>> board1
     { 5, 0, 3 }
 };
 
+static vector<vector<int>> board2 
+{
+    { 1, 2, 3 },
+    { 5, 0, 3 }
+};
+
 enum Direction { NORTH=-1, SOUTH=1, EAST=2, WEST=-2, STOP=0 };
 
 namespace Solution {
     class Program {
         public:
             int slidingPuzzle(vector<vector<int>>& board);
-            void BFS(vector<vector<int>> currentBoard, unordered_map<string, string>& visited, pair<int,int> zeroPos, Direction dir);
-            void Swap (vector<vector<int>>& currentBoard, pair<int,int>& zeroPos, Direction dir);
+            void BFS(queue<vector<vector<int>>>& bfsQ, vector<vector<int>>& currentBoard, pair<int,int> zeroPos, unordered_map<string, int>& visited);
+            void Swap (vector<vector<int>>& currentBoard, vector<vector<int>>& tempBoard, pair<int,int> zeroPos, Direction dir);
             string Stringify(vector<vector<int>>& currentBoard);
     };
 
     int Program::slidingPuzzle(vector<vector<int>>& board) {
         pair<int, int> zeroPos;
-        unordered_map<string, string> visited; // map will store as visited[currentstate] = parentState
+        unordered_map<string, int> visited; // map will store as visited[currentstate] = parentState
         vector<vector<int>> currentBoard (board);
-        
+        queue< vector<vector<int>> > bfsQ;
         string target = "123450";
         string start = "";
         for(int i=0 ; i<board.size(); i++) {
             for (int j=0; j<board[i].size(); j++) {
                 if (board[i][j] == 0) zeroPos = make_pair(i, j);
-                start += to_string(board[i][j]);
+                string start = "";
             }
         }
-
+        bfsQ.push(currentBoard);
+        visited[this->Stringify(currentBoard)] = 0;
         if (start == target) return 0;
-        
-        BFS(currentBoard, visited, zeroPos, STOP);
-        if (!visited.contains(target)) return -1;
 
         int moves = 0;
-        while (start != target) {
-            target = visited[target];
-            cout << target << endl;
-            moves++;
+        while (!bfsQ.empty()) {
+            currentBoard = bfsQ.front();
+            if (this->Stringify(bfsQ.front()) == target) break;
+
+            // Find Zero Position
+            for(int i=0 ; i<currentBoard.size(); i++)
+                for (int j=0; j<currentBoard[i].size(); j++)
+                    if (currentBoard[i][j] == 0) zeroPos = make_pair(i, j);
+            
+            // BFS Search
+            BFS(bfsQ, currentBoard, zeroPos, visited);
+            bfsQ.pop();
         }
-        return moves;
+        if (!visited.contains(target)) return -1;
+        return visited[target];
     }
 
     string Program::Stringify(vector<vector<int>>& board) {
@@ -70,33 +83,46 @@ namespace Solution {
         return s;
     }
 
-    // Swap zero around, and save this as a state along with the zero position,
-    // For each state, you want to swap again in the 4 adjacent cells (north,south,west,east), save this as a state as well
-    // And just keep repeating till we find the target state
-    void Program::BFS(vector<vector<int>> currentBoard, unordered_map<string, string>& visited, pair<int,int> zeroPos, Direction dir) {
-        string target = "123450";
-        // If at any point the target is found stop BFS
-        if (visited.contains(target)) return;
-        
+    void Program::BFS(queue< vector<vector<int>> >& bfsQ, vector<vector<int>>& currentBoard, pair<int,int> zeroPos, unordered_map<string, int>& visited)
+    {
         vector<vector<int>> tempBoard (currentBoard);
-        this->Swap(tempBoard, zeroPos, dir);
-        string s = "";
-        for(int i=0 ; i<tempBoard.size(); i++)
-            for (int j=0; j<tempBoard[i].size(); j++)
-                s += to_string(tempBoard[i][j]);
-        
-        if (visited.contains(s)) return;
-        visited[s] = this->Stringify(currentBoard);
-
-        // Traverse States
-        if (zeroPos.first-1 >= 0) this->BFS(tempBoard, visited, zeroPos, NORTH);
-        if (zeroPos.first+1 < tempBoard.size()) this->BFS(tempBoard, visited, zeroPos, SOUTH);
-        if (zeroPos.second-1 >= 0) this->BFS(tempBoard, visited, zeroPos, WEST);
-        if (zeroPos.second+1 < tempBoard[0].size()) this->BFS(tempBoard, visited, zeroPos, EAST);
+        string s;
+        if (zeroPos.first-1 >= 0) {
+            this->Swap(currentBoard, tempBoard, zeroPos, NORTH);
+            s = this->Stringify(tempBoard);
+            if (!visited.contains(s)) {
+                bfsQ.push(vector<vector<int>> (tempBoard));
+                visited[s] = visited[this->Stringify(currentBoard)]+1;
+            }
+        }
+        if (zeroPos.first+1 < currentBoard.size()) {
+            this->Swap(currentBoard, tempBoard, zeroPos, SOUTH);
+            s = this->Stringify(tempBoard);
+            if (!visited.contains(s)) {
+                bfsQ.push(vector<vector<int>> (tempBoard));
+                visited[s] = visited[this->Stringify(currentBoard)]+1;
+            }
+        }
+        if (zeroPos.second-1 >= 0) {
+            this->Swap(currentBoard, tempBoard, zeroPos, WEST);
+            s = this->Stringify(tempBoard);
+            if (!visited.contains(s)) {
+                bfsQ.push(vector<vector<int>> (tempBoard));
+                visited[s] = visited[this->Stringify(currentBoard)]+1;
+            }
+        }
+        if (zeroPos.second+1 < currentBoard[0].size()) {
+            this->Swap(currentBoard, tempBoard, zeroPos, EAST);
+            s = this->Stringify(tempBoard);
+            if (!visited.contains(s)) {
+                bfsQ.push(vector<vector<int>> (tempBoard));
+                visited[s] = visited[this->Stringify(currentBoard)]+1;
+            }
+        }
         return;
     }
 
-    void Program::Swap(vector<vector<int>>& currentBoard, pair<int,int>& zeroPos, Direction dir) {
+    void Program::Swap(vector<vector<int>>& currentBoard, vector<vector<int>>& tempBoard, pair<int,int> zeroPos, Direction dir) {
         // dir = 1, -1 swap down or up
         // dir = 2, -2 swap left or right
         vector<vector<int>> copy (currentBoard);
@@ -109,8 +135,7 @@ namespace Solution {
         int swap = copy[newPos.first][newPos.second];
         copy[zeroPos.first][zeroPos.second] = swap;
         copy[newPos.first][newPos.second] = 0;
-        currentBoard = copy;
-        zeroPos = newPos;
+        tempBoard = copy;
         return;
     }
 }
@@ -120,5 +145,6 @@ int main () {
     Program obj;
     cout << obj.slidingPuzzle(board) << endl;
     cout << obj.slidingPuzzle(board1) << endl;
+    cout << obj.slidingPuzzle(board2) << endl;
     return 0;
 }
