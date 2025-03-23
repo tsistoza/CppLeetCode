d0a0bb0e5be0273726ec5874b0eb6b515aad6199
Vinton Sistoza <tsistoza@gmail.com>
2025-03-23 12:41:34 -1000

    HouseRobber.cpp 198
    LeetCode 198

---
+++ HouseRobber.cpp
@@ -0,0 +1,36 @@
+// LeetCode 198
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using std::cout, std::endl, std::vector;
+using std::max;
+
+static vector<int> nums { 40, 2, 7, 6, 50 };
+
+namespace Solution {
+    class Program {
+        public:
+            int rob(vector<int>& nums);
+    };
+
+    int Program::rob(vector<int>& nums) {
+        vector<int> dp(nums.size(), 0);
+
+        for (int i=0; i<nums.size(); i++) {
+            if (i == 0 || i == 1) {
+                dp[i] = nums[i];
+                continue;
+            }
+            dp[i] = max(dp[i-2], dp[i-3]) + nums[i];
+        }
+
+        return max(dp.back(), dp[dp.size()-2]);
+    }
+}
+
+int main() {
+    using namespace Solution;
+    Program obj;
+    cout << obj.rob(nums) << endl;
+}
